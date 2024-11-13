439600a428ec4dc5e1ad14078feda20e865653fe
Yassinean <yassinedubraska@gmail.com>
2024-11-13 21:56:08 +0100

    BK-8 adding driver mapper

--- bookride/src/main/java/com/bookride/mapper/DriverMapper.java
+++ bookride/src/main/java/com/bookride/mapper/DriverMapper.java
@@ -5,8 +5,6 @@ import org.mapstruct.Mapping;
 import com.bookride.dto.DriverDto;
 import com.bookride.model.Driver;
 
-import liquibase.pro.packaged.M;
-
 public interface  DriverMapper {
     DriverDto toDto(Driver driver);
     Driver toEntity(DriverDto driverDto);
